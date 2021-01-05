543feac76e026c370571dd54dc80f33661d0da90
unknown <alinh1803@gmail.com>
2021-01-05 09:11:06 +0700

    update github page

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import AccountAction from './containers/AccountAction/AccountAction';
+import { HashRouter, Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import history from './history/history'
+
+function App() {
+
+  return (
+    <Router history={history}> 
+        <div className="App">
+          <Switch>
+          <Route path="/account" component={AccountAction}/>
+            <Route path="/" component={HomePage}/>
+          </Switch>
+        </div>
+    </Router>
+
+  );
+
+}
+
+
+
+export default App;
